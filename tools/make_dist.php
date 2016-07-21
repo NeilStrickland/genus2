54b8d10bd47636f681c1ef934c61d7cc0656be94
Neil Strickland <N.P.Strickland@sheffield.ac.uk>
2016-07-21 16:52:06 +0100

    Exit on notices or warnings Definition of $map moved out to genus2.inc Added cp_r() Corrected $arxiv_dir to $map->arxiv_dir in various places Marked up Maple code files now have names like foo.mpl.html instead of foo.mpl Various other changes

--- tools/make_dist.php
+++ tools/make_dist.php
@@ -1,15 +1,26 @@
 <?php
 
-$map = new stdClass();
+require_once('genus2.inc');
 
-$map->genus2_dir     = dirname(dirname(__DIR__));
-$map->main_dir       = $map->genus2_dir . '/main';
-$map->images_dir     = $map->main_dir . '/images';
-$map->latex_dir      = $map->main_dir . '/latex';
-$map->maple_dir      = $map->main_dir . '/maple';
-$map->worksheets_dir = $map->main_dir . '/worksheets';
-$map->doc_dir        = $map->main_dir . '/doc';
- 
+// Exit on notices or warnings
+
+function errHandle($errNo, $errStr, $errFile, $errLine) {
+ $msg = "$errStr in $errFile on line $errLine";
+ if ($errNo == E_NOTICE || $errNo == E_WARNING) {
+  throw new ErrorException($msg, $errNo);
+ } else {
+  echo $msg;
+ }
+}
+
+set_error_handler('errHandle');
+
+//////////////////////////////////////////////////////////////////////
+
+ini_set('memory_limit',-1);
+
+//////////////////////////////////////////////////////////////////////
+
 function rm_rf($dir) {
  if (! $dir) { return(false); }
  if (! is_dir($dir)) { return(false); }
@@ -34,6 +45,47 @@ function rm_rf($dir) {
 
  return true;
 }
+
+//////////////////////////////////////////////////////////////////////
+
+function cp_r($src,$dst) { 
+ $dir = opendir($src); 
+ if (! is_dir($dst)) { mkdir($dst); }
+ while(false !== ( $file = readdir($dir)) ) { 
+  if (( $file != '.' ) && ( $file != '..' )) { 
+   if ( is_dir($src . '/' . $file) ) { 
+    cp_r($src . '/' . $file,$dst . '/' . $file); 
+   } 
+   else { 
+    copy($src . '/' . $file,$dst . '/' . $file); 
+   } 
+  } 
+ } 
+ closedir($dir); 
+} 
+
+//////////////////////////////////////////////////////////////////////
+
+function make_archives($src_dir,$dest_dir,$file) {
+ global $map;
+
+ $full_file = $dest_dir . '/' . $file;
+
+ $phar = new PharData($full_file . '.tar');
+ $phar->buildFromDirectory($src_dir);
+ $phar->compress(Phar::GZ);
+ $phar->stopBuffering();
+ $phar = null;
+ sleep(2);
+ unlink($full_file . '.tar');
+ rename($full_file . '.tar.gz',$full_file . '.tgz');
+
+ $phar = new PharData($full_file . '.zip');
+ $phar->buildFromDirectory($src_dir);
+
+}
+
+//////////////////////////////////////////////////////////////////////
 
 $map->subdirs =
  array(
@@ -228,6 +280,38 @@ $map->files['worksheets'] =
        '/worksheets/build_data_toy.mw',
        );
 
+$map->files['data'] =
+ array(
+       '/data/embedded/roothalf/EH_atlas.m',
+       '/data/embedded/roothalf/E_to_S_map.m',
+       '/data/embedded/roothalf/quadrature_frobenius_256a.m',
+       '/data/embedded/roothalf/quadrature_frobenius_25a.m',
+       '/data/embedded/roothalf/rational_grid_dunavant_19.m',
+       '/data/embedded/roothalf/split_rational_grid_wx_30.m',
+       '/data/embedded/roothalf/square_diffeo_E0_inverse.m',
+       '/data/hyperbolic/automorphy_system.m',
+       '/data/hyperbolic/HP_table.m',
+       '/data/hyperbolic/H_to_P_map.m',
+       '/data/hyperbolic/P_to_H_map.m',
+       '/data/quadrature/dunavant_19.m',
+       '/data/quadrature/wandzurat_xiao_30.m',
+       );
+
+$map->files['data_toy'] =
+ array(
+       '/data_toy/embedded/roothalf/EH_atlas.m',
+       '/data_toy/embedded/roothalf/E_to_S_map.m',
+       '/data_toy/embedded/roothalf/quadrature_frobenius_25a.m',
+       '/data_toy/embedded/roothalf/rational_grid_dunavant_19.m',
+       '/data_toy/embedded/roothalf/square_diffeo_E0_inverse.m',
+       '/data_toy/hyperbolic/automorphy_system.m',
+       '/data_toy/hyperbolic/HP_table.m',
+       '/data_toy/hyperbolic/H_to_P_map.m',
+       '/data_toy/hyperbolic/P_to_H_map.m',
+       '/data_toy/quadrature/dunavant_19.m',
+       '/data_toy/quadrature/wandzurat_xiao_30.m',
+       );
+
 $map->classes =
  array(
        'net',
@@ -258,13 +342,14 @@ $map->classes =
        'automorphy_system',
        );
 
+//////////////////////////////////////////////////////////////////////
+
 function make_arxiv() {
  global $map;
  
- $arxiv_dir = $map->genus2_dir . '/arxiv';
- rm_rf($arxiv_dir);
- mkdir($arxiv_dir);
-
+ rm_rf($map->arxiv_dir);
+ mkdir($map->arxiv_dir);
+ 
  $tex = file_get_contents($map->main_dir . '/latex/genus2.tex');
 
  // All included tikz files have now been inlined, so the code below is
@@ -287,14 +372,14 @@ function make_arxiv() {
 		     file_get_contents($map->latex_dir . '/genus2.bbl'),
 		     $tex);
  
- file_put_contents($arxiv_dir . '/genus2.tex',$tex);
+ file_put_contents($map->arxiv_dir . '/genus2.tex',$tex);
 
- mkdir($arxiv_dir . '/images');
+ mkdir($map->arxiv_dir . '/images');
  foreach($map->files['included_images'] as $f) {
-  copy($map->main_dir . $f,$arxiv_dir . $f);
+  copy($map->main_dir . $f,$map->arxiv_dir . $f);
  }
 
- $anc_dir = $arxiv_dir . '/anc';
+ $anc_dir = $map->arxiv_dir . '/anc';
  
  mkdir($anc_dir);
  mkdir($anc_dir . '/doc');
@@ -324,7 +409,8 @@ function make_arxiv() {
  
  foreach($map->files['all_maple'] as $f) {
   copy($map->main_dir . $f,$anc_dir . $f);
-  copy($map->main_dir . '/doc/' . $f,$anc_dir . '/doc/' . $f);
+  copy($map->main_dir . '/doc/' . $f . '.html',
+             $anc_dir . '/doc/' . $f . '.html');
  }
 
  foreach($map->classes as $c) {
@@ -339,70 +425,195 @@ function make_arxiv() {
        $anc_dir . '/worksheets/' . $b);
   
  }
+
+ make_archives($map->arxiv_dir,$map->dist_dir,'arxiv');
 }
+
+//////////////////////////////////////////////////////////////////////
 
 function make_thin_dist() {
  global $map;
  
- $thin_dist_dir = $map->genus2_dir . '/thin_dist';
- rm_rf($thin_dist_dir);
+ rm_rf($map->thin_dist_dir);
   
- mkdir($thin_dist_dir);
- mkdir($thin_dist_dir . '/doc');
- mkdir($thin_dist_dir . '/doc/maple');
- mkdir($thin_dist_dir . '/images');
- mkdir($thin_dist_dir . '/latex');
- mkdir($thin_dist_dir . '/latex/tikz_includes');
- mkdir($thin_dist_dir . '/maple');
- mkdir($thin_dist_dir . '/worksheets');
+ mkdir($map->thin_dist_dir);
+ mkdir($map->thin_dist_dir . '/doc');
+ mkdir($map->thin_dist_dir . '/doc/maple');
+ mkdir($map->thin_dist_dir . '/images');
+ mkdir($map->thin_dist_dir . '/plots');
+ mkdir($map->thin_dist_dir . '/latex');
+ mkdir($map->thin_dist_dir . '/latex/tikz_includes');
+ mkdir($map->thin_dist_dir . '/maple');
+ mkdir($map->thin_dist_dir . '/worksheets');
 
- mkdir($thin_dist_dir . '/maple/domain');
- mkdir($thin_dist_dir . '/maple/quadrature');
- mkdir($thin_dist_dir . '/maple/projective');
- mkdir($thin_dist_dir . '/maple/hyperbolic');
- mkdir($thin_dist_dir . '/maple/embedded');
- mkdir($thin_dist_dir . '/maple/embedded/roothalf');
+ mkdir($map->thin_dist_dir . '/maple/domain');
+ mkdir($map->thin_dist_dir . '/maple/quadrature');
+ mkdir($map->thin_dist_dir . '/maple/projective');
+ mkdir($map->thin_dist_dir . '/maple/hyperbolic');
+ mkdir($map->thin_dist_dir . '/maple/embedded');
+ mkdir($map->thin_dist_dir . '/maple/embedded/roothalf');
 
- mkdir($thin_dist_dir . '/doc/maple/domain');
- mkdir($thin_dist_dir . '/doc/maple/quadrature');
- mkdir($thin_dist_dir . '/doc/maple/projective');
- mkdir($thin_dist_dir . '/doc/maple/hyperbolic');
- mkdir($thin_dist_dir . '/doc/maple/embedded');
- mkdir($thin_dist_dir . '/doc/maple/embedded/roothalf');
- mkdir($thin_dist_dir . '/doc/classes');
+ mkdir($map->thin_dist_dir . '/doc/maple/domain');
+ mkdir($map->thin_dist_dir . '/doc/maple/quadrature');
+ mkdir($map->thin_dist_dir . '/doc/maple/projective');
+ mkdir($map->thin_dist_dir . '/doc/maple/hyperbolic');
+ mkdir($map->thin_dist_dir . '/doc/maple/embedded');
+ mkdir($map->thin_dist_dir . '/doc/maple/embedded/roothalf');
+ mkdir($map->thin_dist_dir . '/doc/classes');
 
  foreach($map->files['doc'] as $f) {
-  copy($map->main_dir . $f,$thin_dist_dir . $f);
+  copy($map->main_dir . $f,$map->thin_dist_dir . $f);
  }
 
  foreach($map->classes as $c) {
   copy($map->main_dir . '/doc/classes/' . $c . '.html',
-       $thin_dist_dir . '/doc/classes/' . $c . '.html'
+       $map->thin_dist_dir . '/doc/classes/' . $c . '.html'
        );
  }
 
  foreach($map->files['included_images'] as $f) {
-  copy($map->main_dir . $f,$thin_dist_dir . $f);
+  copy($map->main_dir . $f,$map->thin_dist_dir . $f);
  }
 
  foreach($map->files['latex'] as $f) {
-  copy($map->main_dir . $f,$thin_dist_dir . $f);
+  copy($map->main_dir . $f,$map->thin_dist_dir . $f);
  }
 
  foreach($map->files['all_maple'] as $f) {
-  copy($map->main_dir . $f,$thin_dist_dir . $f);
-  copy($map->main_dir . '/doc/' . $f,$thin_dist_dir . '/doc/' . $f);
+  copy($map->main_dir . $f,$map->thin_dist_dir . $f);
+  copy($map->main_dir . '/doc/' . $f . '.html',
+       $map->thin_dist_dir . '/doc/' . $f . '.html');
  }
 
  foreach($map->files['worksheets'] as $f) {
   $b = basename($f);
   copy($map->main_dir . '/worksheets/small/' . $b,
-       $thin_dist_dir . '/worksheets/' . $b);
+       $map->thin_dist_dir . '/worksheets/' . $b);
   
  }
 }
 
+//////////////////////////////////////////////////////////////////////
+
+function make_fat_dist() {
+ global $map;
+ 
+ rm_rf($map->fat_dist_dir);
+  
+ mkdir($map->fat_dist_dir);
+ mkdir($map->fat_dist_dir . '/doc');
+ mkdir($map->fat_dist_dir . '/doc/maple');
+ mkdir($map->fat_dist_dir . '/images');
+ mkdir($map->fat_dist_dir . '/plots');
+ mkdir($map->fat_dist_dir . '/latex');
+ mkdir($map->fat_dist_dir . '/latex/tikz_includes');
+ mkdir($map->fat_dist_dir . '/maple');
+ mkdir($map->fat_dist_dir . '/worksheets');
+ mkdir($map->fat_dist_dir . '/data');
+ mkdir($map->fat_dist_dir . '/data_toy');
+
+ mkdir($map->fat_dist_dir . '/maple/domain');
+ mkdir($map->fat_dist_dir . '/maple/quadrature');
+ mkdir($map->fat_dist_dir . '/maple/projective');
+ mkdir($map->fat_dist_dir . '/maple/hyperbolic');
+ mkdir($map->fat_dist_dir . '/maple/embedded');
+ mkdir($map->fat_dist_dir . '/maple/embedded/roothalf');
+
+ mkdir($map->fat_dist_dir . '/data/domain');
+ mkdir($map->fat_dist_dir . '/data/quadrature');
+ mkdir($map->fat_dist_dir . '/data/projective');
+ mkdir($map->fat_dist_dir . '/data/hyperbolic');
+ mkdir($map->fat_dist_dir . '/data/embedded');
+ mkdir($map->fat_dist_dir . '/data/embedded/roothalf');
+
+ mkdir($map->fat_dist_dir . '/data_toy/domain');
+ mkdir($map->fat_dist_dir . '/data_toy/quadrature');
+ mkdir($map->fat_dist_dir . '/data_toy/projective');
+ mkdir($map->fat_dist_dir . '/data_toy/hyperbolic');
+ mkdir($map->fat_dist_dir . '/data_toy/embedded');
+ mkdir($map->fat_dist_dir . '/data_toy/embedded/roothalf');
+
+ mkdir($map->fat_dist_dir . '/doc/maple/domain');
+ mkdir($map->fat_dist_dir . '/doc/maple/quadrature');
+ mkdir($map->fat_dist_dir . '/doc/maple/projective');
+ mkdir($map->fat_dist_dir . '/doc/maple/hyperbolic');
+ mkdir($map->fat_dist_dir . '/doc/maple/embedded');
+ mkdir($map->fat_dist_dir . '/doc/maple/embedded/roothalf');
+ mkdir($map->fat_dist_dir . '/doc/classes');
+
+ foreach($map->files['doc'] as $f) {
+  copy($map->main_dir . $f,$map->fat_dist_dir . $f);
+ }
+
+ foreach($map->classes as $c) {
+  copy($map->main_dir . '/doc/classes/' . $c . '.html',
+       $map->fat_dist_dir . '/doc/classes/' . $c . '.html'
+       );
+ }
+
+ foreach($map->files['included_images'] as $f) {
+  copy($map->main_dir . $f,$map->fat_dist_dir . $f);
+ }
+
+ foreach($map->files['latex'] as $f) {
+  copy($map->main_dir . $f,$map->fat_dist_dir . $f);
+ }
+
+ foreach($map->files['all_maple'] as $f) {
+  copy($map->main_dir . $f,$map->fat_dist_dir . $f);
+  copy($map->main_dir . '/doc/' . $f . '.html',
+       $map->fat_dist_dir . '/doc/' . $f . '.html');
+ }
+
+ foreach($map->files['worksheets'] as $f) {
+  copy($map->main_dir . $f, $map->fat_dist_dir . $f);
+ }
+
+ foreach($map->files['data'] as $f) {
+  copy($map->main_dir . $f, $map->fat_dist_dir . $f);
+ }
+
+ foreach($map->files['data_toy'] as $f) {
+  copy($map->main_dir . $f, $map->fat_dist_dir . $f);
+ }
+
+ cp_r($map->images_dir,$map->fat_dist_dir . '/images'); 
+ cp_r($map->plots_dir ,$map->fat_dist_dir . '/plots'); 
+ cp_r($map->tikz_dir  ,$map->fat_dist_dir . '/latex/tikz_includes'); 
+
+}
+
+//////////////////////////////////////////////////////////////////////
+
+function make_page_dir() {
+ global $map;
+
+ copy($map->latex_dir . '/genus2.pdf',$map->page_dir . '/genus2.pdf');
+
+ $dir = $map->page_dir . '/doc';
+ if (is_dir($dir)) { rm_rf($dir); }
+ cp_r($map->doc_dir,$dir);
+
+ $dir = $map->page_dir . '/images';
+ if (is_dir($dir)) { rm_rf($dir); }
+ cp_r($map->images_dir,$dir);
+
+}
+ 
+//////////////////////////////////////////////////////////////////////
+
+function make_dist_archives() {
+ global $map;
+
+ make_archives($map->thin_dist_dir,$map->dist_dir,'genus2_thin');
+ make_archives($map->fat_dist_dir ,$map->dist_dir,'genus2_fat');
+}
+
+//////////////////////////////////////////////////////////////////////
+
 make_arxiv();
 make_thin_dist();
+make_fat_dist();
+make_page_dir();
 
 ?>
